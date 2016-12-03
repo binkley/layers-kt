6c4a9a92744b6f822ec5dfe2234ae630e4e2c03a
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2016-12-03 15:38:33 -0600

    Prep for weight/volume on item

---
+++ layers-kt-domain/src/main/kotlin/hm/binkley/layers/dnd/Fraction.kt
@@ -0,0 +1,50 @@
+package hm.binkley.layers.dnd
+
+import java.lang.Math.abs
+import java.math.BigDecimal
+import java.math.RoundingMode.HALF_UP
+
+abstract class Fraction<F : Fraction<F>>(private val ctor: (Int, Int) -> F,
+        numerator: Int, denominator: Int) {
+    private val numerator: Int
+    private val denominator: Int
+
+    init {
+        if (numerator < 0 || denominator < 1)
+            throw IllegalArgumentException()
+        val gcm = gcm(numerator, denominator)
+        this.numerator = abs(numerator / gcm)
+        this.denominator = abs(denominator / gcm)
+    }
+
+    fun add(that: F): F {
+        val numerator = this.numerator * that.denominator + that.numerator * denominator
+        val denominator = this.denominator * that.denominator
+        return ctor.invoke(numerator, denominator)
+    }
+
+    override fun toString(): String {
+        return BigDecimal.valueOf(numerator.toLong()).
+                divide(BigDecimal.valueOf(denominator.toLong()), 1, HALF_UP).
+                toString()
+    }
+
+    override final fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as Fraction<*>
+        if (numerator != other.numerator) return false
+        if (denominator != other.denominator) return false
+        return true
+    }
+
+    override final fun hashCode(): Int = 31 * numerator + denominator
+
+    companion object {
+        private fun gcm(numerator: Int, denominator: Int): Int
+                = when (denominator) {
+            0 -> numerator
+            else -> gcm(denominator, numerator % denominator)
+        }
+    }
+}
---
+++ layers-kt-domain/src/test/kotlin/hm/binkley/layers/dnd/FractionTest.kt
@@ -0,0 +1,19 @@
+package hm.binkley.layers.dnd
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class FractionTest {
+    @Test
+    fun shouldDiplayNicely() {
+        assertEquals("0.7", EgFraction(2, 3).toString())
+    }
+
+    @Test
+    fun shouldAdd() {
+        assertEquals(EgFraction(3, 2), EgFraction(5, 6).add(EgFraction(2, 3)))
+    }
+
+    internal class EgFraction(numerator: Int, denominator: Int)
+        : Fraction<EgFraction>(::EgFraction, numerator, denominator)
+}

d29b7ff39fa5247ea3079255bc38acb444a4b691
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-08-05 07:09:06 -0500

    Demonstrate (disabled) design flaw

--- layers-kt-lib/src/test/kotlin/hm/binkley/layers/LayersTest.kt
+++ layers-kt-lib/src/test/kotlin/hm/binkley/layers/LayersTest.kt
@@ -1,22 +1,36 @@
 package hm.binkley.layers
 
 import hm.binkley.layers.Layers.LayerSurface
+import hm.binkley.layers.rules.Rule.Companion.sumAll
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class LayersTest : LayersTestSupport<ScratchLayer>(::ScratchLayer) {
     @Test
-    fun shouldChainSavingAndAssignToSuperType() {
-        @Suppress("UNUSED_VARIABLE")
-        val layer: SubLayer<*> = firstLayer.saveAndNext(::ScratchLayer)
-            .saveAndNext(::FinalLayer)
+    fun `should chain saving and assign to super type`() {
+        firstLayer.saveAndNext(::ScratchLayer).saveAndNext(::FinalLayer)
     }
 
     @Test
-    fun shouldHaveHistory() {
+    fun `should have history`() {
         firstLayer.saveAndNext(::ScratchLayer)
 
         assertEquals(listOf(firstLayer), layers.layers())
+    }
+
+    @Disabled("TODO: Warning is correct: Delegating to 'var' property" +
+            " does not take its changes into account")
+    @Test
+    fun `should reject updates after saved`() {
+        firstLayer.put("A", sumAll())
+        val secondLayer = firstLayer.saveAndNext(::ScratchLayer)
+        secondLayer.saveAndNext(::FinalLayer)
+
+        assertThrows<IllegalStateException> {
+            secondLayer.put("A", 1)
+        }
     }
 
     open class SubLayer<L : SubLayer<L>>(layers: LayerSurface, name: String) :

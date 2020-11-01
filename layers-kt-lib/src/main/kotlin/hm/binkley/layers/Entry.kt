4eca6902c7f3fc2ff4d1b12d56354954e736d55f
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-11-01 10:48:18 -0600

    Docs

--- layers-kt-lib/src/main/kotlin/hm/binkley/layers/Entry.kt
+++ layers-kt-lib/src/main/kotlin/hm/binkley/layers/Entry.kt
@@ -1,30 +1,39 @@
 package hm.binkley.layers
 
 /**
- * A layer map entry, distinct from a
+ * A layer entry is distinct from a
  * [`Map.Entry`](https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/-map/-entry/)
  * or [`Pair`](https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-pair/) in
  * that:
- * * The "key" is only sometimes applicable
- * * The "value" might not be computed until provided an argument
- *
- * Note that `Pair` is a data class, and requires fixed key/value arguments in
- * its constructor; `Map.Entry` is an interface, and more flexible in this
- * context.
+ * * "key" is not applicable to `Value` which violates the contract of
+ *   `Map.Entry` and `Pair.first`
+ * * "value" is not computed for `Rule` until provided an argument which
+ *   violates the contract of `Map.Entry` and `Pair.second`
  *
  * @see Rule which computes "value" from arguments, and makes use of "key"
  * @see Value which has a fixed value for "value", and ignores "key
+ *
+ * @todo Merge/encapsulate via `ruleCalculation.kt`?
  */
 sealed class Entry<T> {
     abstract override fun toString(): String
 }
 
+/**
+ * A direct value in an individual layer entry without regard to the key
+ * used to lookup this value.
+ */
 data class Value<T>(
     val value: T,
 ) : Entry<T>() {
     override fun toString() = "Value: $value"
 }
 
+/**
+ * A computed value for layers based on all direct values over each
+ * individual layer. The computation may consider the "key" for the value
+ * requested.
+ */
 abstract class Rule<T>(
     protected val key: String,
 ) : Entry<T>() {

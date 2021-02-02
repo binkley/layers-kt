e0174c2a2fc5c9476fa9f68b7056caf2a8a8f714
Brian Oxley <boxley@thoughtworks.com>
2021-02-02 08:16:33 +0800

    Naming

--- layers-kt-domain/src/main/kotlin/hm/binkley/layers/dnd/Dnd.kt
+++ layers-kt-domain/src/main/kotlin/hm/binkley/layers/dnd/Dnd.kt
@@ -1,17 +1,17 @@
-package hm.binkley.layers.domain
+package hm.binkley.layers.dnd
 
 import hm.binkley.layers.MutablePlainLayer
 import lombok.Generated
 import java.util.Objects.hash
 
-class Domain(
+class Dnd(
     name: String,
     val fakeForMutation: Boolean,
 ) : MutablePlainLayer(name) {
     @Generated
     override fun equals(other: Any?) = this === other ||
         super.equals(other) &&
-        other is Domain &&
+        other is Dnd &&
         fakeForMutation == other.fakeForMutation
 
     override fun hashCode() = 31 * super.hashCode() + hash(fakeForMutation)

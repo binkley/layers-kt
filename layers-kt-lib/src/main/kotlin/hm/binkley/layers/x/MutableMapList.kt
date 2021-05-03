db6cdedf581ba079faaacd452f987ffb29add16f
Brian Oxley <boxley@thoughtworks.com>
2021-05-03 08:20:14 +0800

    Tests pass for alternative

--- layers-kt-lib/src/main/kotlin/hm/binkley/layers/x/MutableMapList.kt
+++ layers-kt-lib/src/main/kotlin/hm/binkley/layers/x/MutableMapList.kt
@@ -9,32 +9,33 @@ class MutableMapList<K, V>(
     MutableList<MutableMap<K, ValueOrRule<V>>> by history {
     init {
         // TODO: Always start with 1 map?
-        if (history.isEmpty()) push(mutableMapOf())
+        if (history.isEmpty()) add(mutableMapOf())
     }
 
-    private val top: MutableMap<K, ValueOrRule<V>> get() = history.first()
+    private val current: MutableMap<K, ValueOrRule<V>> get() = history.last()
 
     fun view() = object : AbstractMap<K, V>() {
         override val entries: Set<Entry<K, V>>
             get() = computeEntries()
     }
 
-    fun edit(block: MutableMap<K, ValueOrRule<V>>.() -> Unit) = top.block()
+    fun edit(block: MutableMap<K, ValueOrRule<V>>.() -> Unit) = current.block()
 
     internal fun computeEntries(): Set<Entry<K, V>> {
         @Suppress("UNCHECKED_CAST")
         fun computeEntry(key: K): Entry<K, V> {
             var rule: Rule<K, V>? = null
             val values: MutableList<V> = mutableListOf()
-            for (map in history) {
+
+            for (map in history.reversed()) {
                 when (val curr: ValueOrRule<V>? = map[key]) {
                     null -> continue
                     is Value<*> -> values += (curr as Value<V>).value
-                    else -> if (null == rule) rule =
-                        curr as Rule<K, V> else continue
+                    else -> if (null == rule) rule = curr as Rule<K, V>
                 }
             }
-            if (null == rule) throw IllegalStateException()
+
+            if (null == rule) throw IllegalStateException("No rule found for '$key'")
             val value =
                 rule(key, values, this) ?: throw IllegalStateException()
 
@@ -49,15 +50,15 @@ class MutableMapList<K, V>(
         return entries
     }
 
-    override val size: Int get() = top.size
-    override fun isEmpty(): Boolean = top.isEmpty()
+    override val size: Int get() = current.size
+    override fun isEmpty(): Boolean = current.isEmpty()
     override val entries: MutableSet<MutableEntry<K, ValueOrRule<V>>> =
-        top.entries
+        current.entries
 
     override fun put(key: K, value: ValueOrRule<V>): ValueOrRule<V>? =
-        top.put(key, value)
+        current.put(key, value)
 
-    override fun clear() = top.clear()
+    override fun clear() = current.clear()
 
     override fun toString() = history.mapIndexed { i, it ->
         "$i: $it"
@@ -65,10 +66,6 @@ class MutableMapList<K, V>(
 }
 
 interface ValueOrRule<V>
-private data class Value<V>(val value: V) : ValueOrRule<V>
-private abstract class Rule<K, V>
-    : ValueOrRule<V>, (K, List<V>, MutableMapList<K, V>) -> V?
-
-fun <V> MutableList<V>.push(top: V) = add(0, top)
-fun <V> MutableList<V>.pop(): V = removeAt(0)
-fun <V> List<V>.peek(): V? = firstOrNull()
+data class Value<V>(val value: V) : ValueOrRule<V>
+abstract class Rule<K, V> :
+    ValueOrRule<V>, (K, List<V>, MutableMapList<K, V>) -> V?
--- layers-kt-lib/src/test/kotlin/hm/binkley/layers/x/MutableMapListTest.kt
+++ layers-kt-lib/src/test/kotlin/hm/binkley/layers/x/MutableMapListTest.kt
@@ -11,8 +11,38 @@ internal class MutableMapListTest {
 
         maps.size shouldBe 0
         maps.isEmpty().shouldBeTrue()
-        maps.keys.isEmpty().shouldBeTrue()
-        maps.entries.isEmpty().shouldBeTrue()
-        maps.values.isEmpty().shouldBeTrue()
+        maps.entries.size shouldBe 0
+        maps.keys.size shouldBe 0
+        maps.values.size shouldBe 0
+
+        maps.history.size shouldBe 1
+    }
+
+    @Test
+    fun `should follow a rule`() {
+        val testKey = "BOB"
+
+        val maps = MutableMapList<String, Any>()
+        maps.edit {
+            this[testKey] = object : Rule<String, Any>() {
+                override fun invoke(
+                    key: String,
+                    values: List<Any>,
+                    history: MutableMapList<String, Any>,
+                ): Int = values.sumOf { it as Int }
+            }
+        }
+
+        maps.add(mutableMapOf())
+        maps.edit {
+            this[testKey] = Value(1)
+        }
+
+        maps.add(mutableMapOf())
+        maps.edit {
+            this[testKey] = Value(2)
+        }
+
+        maps.view()[testKey] shouldBe 3
     }
 }

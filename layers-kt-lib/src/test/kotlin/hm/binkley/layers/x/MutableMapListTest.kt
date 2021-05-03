70b3af9705ae9afd49475cb1bd234760bb8c4a42
Brian Oxley <boxley@thoughtworks.com>
2021-05-03 08:44:08 +0800

    Coverage

--- layers-kt-lib/src/test/kotlin/hm/binkley/layers/x/MutableMapListTest.kt
+++ layers-kt-lib/src/test/kotlin/hm/binkley/layers/x/MutableMapListTest.kt
@@ -19,18 +19,30 @@ internal class MutableMapListTest {
     }
 
     @Test
+    fun `should clear current`() {
+        val maps = MutableMapList<String, Any>()
+        maps.edit {
+            this["BOB"] = testRule
+        }
+
+        maps.add(mutableMapOf())
+        maps.edit {
+            this["NANCY"] = testRule
+        }
+
+        maps.clear()
+
+        maps.size shouldBe 0
+        maps.view().size shouldBe 1
+    }
+
+    @Test
     fun `should follow a rule`() {
         val testKey = "BOB"
 
         val maps = MutableMapList<String, Any>()
         maps.edit {
-            this[testKey] = object : Rule<String, Any>() {
-                override fun invoke(
-                    key: String,
-                    values: List<Any>,
-                    history: MutableMapList<String, Any>,
-                ): Int = values.sumOf { it as Int }
-            }
+            this[testKey] = testRule
         }
 
         maps.add(mutableMapOf())
@@ -45,4 +57,12 @@ internal class MutableMapListTest {
 
         maps.view()[testKey] shouldBe 3
     }
+}
+
+val testRule = object : Rule<String, Any>() {
+    override fun invoke(
+        key: String,
+        values: List<Any>,
+        history: MutableMapList<String, Any>,
+    ): Int = values.sumOf { it as Int }
 }

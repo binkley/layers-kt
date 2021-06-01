7c097ffec5394baecf5d371f30266097fab3f0c1
Brian Oxley <boxley@thoughtworks.com>
2021-06-01 16:53:25 +0800

    Formatting

--- layers-kt-domain/src/main/kotlin/hm/binkley/layers/rpg/item/GirdleOfHillGiantMight.kt
+++ layers-kt-domain/src/main/kotlin/hm/binkley/layers/rpg/item/GirdleOfHillGiantMight.kt
@@ -7,12 +7,13 @@ import kotlin.math.max
 /**
  * @todo Do not repeat oneself calculating current might sans this item
  * @todo The rule is subtly mistaken.  Say a character had 2 magic items:
- *       1) An earlier item granted 22 strength
- *       2) This later item grants 21 strength
- *       The 22 should prevail.  A smarter rule would try what-if scenarios
+ *       1) An earlier item granted 20 strength
+ *       2) This later item grants 19 strength
+ *       The 20 should prevail.  A smarter rule would try what-if scenarios
  *       comparing without this item, and with this item
  */
-class GirdleOfHillGiantMight : RpgLayer("Girdle of Might of the Hill Giant") {
+class GirdleOfHillGiantMight :
+    RpgLayer("Girdle of Might of the Hill Giant") {
     init {
         edit {
             this[MIGHT.name] =

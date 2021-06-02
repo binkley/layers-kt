ff0b61def9c0aafc28fb46217b701743e2430330
Brian Oxley <boxley@thoughtworks.com>
2021-06-02 09:56:17 +0800

    Layout

---
+++ layers-kt-lib/src/main/kotlin/hm/binkley/layers/DefaultMutableLayer.kt
@@ -0,0 +1,24 @@
+package hm.binkley.layers
+
+open class DefaultMutableLayer<K : Any, V : Any, M : DefaultMutableLayer<K, V, M>>(
+    override val name: String,
+    private val map: MutableMap<K, ValueOrRule<V>> = mutableMapOf(),
+) : MutableLayer<K, V, M>, MutableMap<K, ValueOrRule<V>> by map {
+    companion object {
+        @Suppress("UNCHECKED_CAST")
+        fun <K : Any, V : Any> defaultMutableLayer(
+            name: String,
+        ): DefaultMutableLayer<K, V, *> =
+            DefaultMutableLayer<K, V, DefaultMutableLayer<K, V, *>>(name)
+    }
+
+    override fun edit(block: EditMap<K, V>.() -> Unit): M {
+        DefaultEditMap().block()
+        return self
+    }
+
+    override fun toString(): String = "$name: $map"
+
+    private inner class DefaultEditMap :
+        EditMap<K, V>, MutableMap<K, ValueOrRule<V>> by map
+}
--- layers-kt-lib/src/main/kotlin/hm/binkley/layers/Layer.kt
+++ layers-kt-lib/src/main/kotlin/hm/binkley/layers/Layer.kt
@@ -14,26 +14,3 @@ interface MutableLayer<K : Any, V : Any, M : MutableLayer<K, V, M>> :
     MutableMap<K, ValueOrRule<V>> {
     fun edit(block: EditMap<K, V>.() -> Unit): M
 }
-
-open class DefaultMutableLayer<K : Any, V : Any, M : DefaultMutableLayer<K, V, M>>(
-    override val name: String,
-    private val map: MutableMap<K, ValueOrRule<V>> = mutableMapOf(),
-) : MutableLayer<K, V, M>, MutableMap<K, ValueOrRule<V>> by map {
-    companion object {
-        @Suppress("UNCHECKED_CAST")
-        fun <K : Any, V : Any> defaultMutableLayer(
-            name: String,
-        ): DefaultMutableLayer<K, V, *> =
-            DefaultMutableLayer<K, V, DefaultMutableLayer<K, V, *>>(name)
-    }
-
-    override fun edit(block: EditMap<K, V>.() -> Unit): M {
-        DefaultEditMap().block()
-        return self
-    }
-
-    override fun toString(): String = "$name: $map"
-
-    private inner class DefaultEditMap :
-        EditMap<K, V>, MutableMap<K, ValueOrRule<V>> by map
-}

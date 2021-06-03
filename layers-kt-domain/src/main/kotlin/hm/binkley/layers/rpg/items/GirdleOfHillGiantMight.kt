5d714585e9cec8000601012388eee969b0db18cd
Brian Oxley <boxley@thoughtworks.com>
2021-06-03 17:29:21 +0800

    Introduce Item

--- layers-kt-domain/src/main/kotlin/hm/binkley/layers/rpg/items/GirdleOfHillGiantMight.kt
+++ layers-kt-domain/src/main/kotlin/hm/binkley/layers/rpg/items/GirdleOfHillGiantMight.kt
@@ -1,12 +1,11 @@
 package hm.binkley.layers.rpg.items
 
-import hm.binkley.layers.rpg.RpgLayer
 import hm.binkley.layers.rpg.RpgLayersEditMap
 import hm.binkley.layers.rpg.Stat.MIGHT
 import hm.binkley.layers.rpg.rules.FloorRule
 
 class GirdleOfHillGiantMight(layers: RpgLayersEditMap) :
-    RpgLayer("Girdle of Might of the Hill Giant") {
+    Item("Girdle of Might of the Hill Giant") {
     init {
         edit {
             this[MIGHT.name] = FloorRule(19, layers)
---
+++ layers-kt-domain/src/main/kotlin/hm/binkley/layers/rpg/items/Item.kt
@@ -0,0 +1,5 @@
+package hm.binkley.layers.rpg.items
+
+import hm.binkley.layers.rpg.RpgLayer
+
+open class Item(name: String) : RpgLayer(name)

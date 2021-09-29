99fe3056de5054024e8b716696cd12bd8bc60f71
Brian Oxley <boxley@thoughtworks.com>
2021-09-29 14:15:08 +0800

    Formatting

--- layers-kt-domain/src/main/kotlin/hm/binkley/layers/rpg/InventoryLayer.kt
+++ layers-kt-domain/src/main/kotlin/hm/binkley/layers/rpg/InventoryLayer.kt
@@ -3,10 +3,9 @@ package hm.binkley.layers.rpg
 class InventoryLayer : RpgLayer<InventoryLayer>("Inventory") {
     init {
         edit {
-            this["ITEM-WEIGHT"] =
-                rule<Float>("Sum[Float]") { _, values, _ ->
-                    values.sum()
-                }
+            this["ITEM-WEIGHT"] = rule<Float>("Sum[Float]") { _, values, _ ->
+                values.sum()
+            }
         }
     }
 }
